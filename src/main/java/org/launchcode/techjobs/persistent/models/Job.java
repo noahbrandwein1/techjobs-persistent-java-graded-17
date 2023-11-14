c7d3d242635354d5aa898e27e1788a587038fb67
noah.brandwein1 <noah.brandwein98@gmail.com>
2023-11-13 21:54:58 -0600

    Test Task 4 pt.1

--- src/main/java/org/launchcode/techjobs/persistent/models/Job.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Job.java
@@ -1,18 +1,24 @@
 package org.launchcode.techjobs.persistent.models;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 
 import java.util.List;
 
 @Entity
 public class Job extends AbstractEntity {
     @ManyToOne
+    @NotNull(message = "Employer is required")
     private Employer employer;
-    private String skills;
+
+    @ManyToMany
+    @JoinTable(
+            name = "job_skills",
+            joinColumns = @JoinColumn(name = "job_id"),
+            inverseJoinColumns = @JoinColumn(name = "skills_id")
+    )
+    private List<Skill> skills;
 
 
     public Job() {
@@ -22,7 +28,7 @@ public class Job extends AbstractEntity {
     public Job(Employer employer, String someSkills) {
         super();
         this.employer = employer;
-        this.skills = someSkills;
+        this.skills = skills;
     }
 
     // Getters and setters.
@@ -43,11 +49,11 @@ public class Job extends AbstractEntity {
         this.employer = employer;
     }
 
-    public String getSkills() {
+    public List<Skill> getSkills() {
         return skills;
     }
 
-    public void setSkills(String skills) {
+    public void setSkills(List<Skill> skills) {
         this.skills = skills;
     }
 
--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,8 +1,10 @@
 package org.launchcode.techjobs.persistent.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import java.util.List;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.repository.CrudRepository;
 
@@ -13,12 +15,23 @@ public class Skill extends AbstractEntity {
     @Size(max = 100, message = "Description must be less than or equal to 100 characters")
     private String description;
 
+    @ManyToMany(mappedBy = "skills")
+    private List<Job> jobs;
+
     public String getDescription() {
         return description;
     }
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+    public void setJobs(List<Job> jobs) {
+        this.jobs = jobs;
     }
 
 }
