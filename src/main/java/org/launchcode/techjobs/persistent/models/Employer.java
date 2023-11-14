2ee447a16f270c7e3fd008cce6d56869d0e164aa
noah.brandwein1 <noah.brandwein98@gmail.com>
2023-11-13 18:30:04 -0600

    Test Task 2 part 1-ish

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,4 +1,38 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+interface EmployerRepository extends CrudRepository<Employer, Integer> {
+
+}
+
+@Entity
 public class Employer extends AbstractEntity {
+
+    @NotBlank
+    @Size(max = 55, message = "Number of characters must be less than or equal to 55")
+    private String location;
+
+    public Employer(){
+
+    }
+
+    public Employer(String name, String location) {
+        setName(name);
+        this.location = location;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
 }
--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,29 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.CrudRepository;
+
+@Repository
+interface SkillRepository extends CrudRepository<Skill, Integer> {
+
+}
+
+@Entity
 public class Skill extends AbstractEntity {
+
+    @NotBlank
+    @Size(max = 100, message = "Description must be less than or equal to 100 characters")
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
 }
